eac5c52cdbcf91958aa256105dee62a3a2a13a7c
Fernanda Tavares <fernanda.r.tavares@avanade.com>
2019-03-30 20:28:17 -0300

    cards recent news added; api news - ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CarouselComponent } from './components/carousel/carousel.component';
 import { HeaderComponent } from './components/header/header.component';
-
 import { DashboardCardComponent } from './components/dashboard-card/dashboard-card.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
-import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 
 @NgModule({
@@ -28,6 +27,8 @@ import { AngularFontAwesomeModule } from 'angular-font-awesome';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
+    HttpClientModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/assets/urls.ts
@@ -0,0 +1,5 @@
+export const urls = {
+  sport: 'https://x0gi02b5m1.execute-api.us-east-1.amazonaws.com/dev/sports',
+  userInfo: 'https://x0gi02b5m1.execute-api.us-east-1.amazonaws.com/dev/users',
+  news: 'https://x0gi02b5m1.execute-api.us-east-1.amazonaws.com/dev/news'
+};
