60c746487c14846904dc95814da46e3fb206459a
Fernanda Tavares <fernanda.r.tavares@avanade.com>
2019-03-31 13:52:59 -0300

    socket send with username logged

--- src/app/pages/news-page/news-page.component.ts
+++ src/app/pages/news-page/news-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SocketService } from 'src/app/shared/service/socket.service';
 import { User, Message } from 'src/app/shared/models/user.model';
+import { SessionStorageService } from 'ngx-webstorage';
 
 @Component({
   selector: 'app-news-page',
@@ -11,20 +12,16 @@ export class NewsPageComponent implements OnInit {
 
   public message: string;
   public comments = [];
+  public user;
 
-  constructor(private _socket: SocketService) { }
+  constructor(
+    private _socket: SocketService,
+    private _sessionStorage: SessionStorageService) { }
 
   ngOnInit() {
+    this.user = this._sessionStorage.retrieve('user');
     this._socket.initSocket();
-    // this._socket.onMessage()
-    // .subscribe((resp) =>{
-    //   this.comments.push(resp);
-    // });
     this._socket.onMessage((resp) => {
-      // const data = {
-      //   type: 'comments',
-      //   message:
-      // };
       this.comments.push(JSON.parse( resp.data));
       console.log(this.comments);
 
@@ -36,9 +33,10 @@ export class NewsPageComponent implements OnInit {
       // const user = new User('Fernanda');
       // const msg = new Message(user, this.message);
       console.log(this.message);
+      const name = this.user ? this.user.name : 'ANONIMO';
       const obj = {
         from: {
-          name:"Waltton",
+          name: name,
         },
         content: this.message
       }
